a280625e97bc9f78397e6e4702f76b1052a870b5
Shiamak Das <shiamakdas@outlook.com>
2026-09-20 21:23:21 -0400

    create input and output dataframes w/ training validation split

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
+# DATAFRAME PREPARATION:
+
 df = pd.read_csv("./data/diabetes_risk_prediction_dataset.csv")
 df = df.drop(columns=['Patient_ID'])
 
 # Numerical Columns
 num_cols = ["Age", "Height_cm", "Weight_kg", "Blood_Glucose", "HbA1c", "Fasting_Blood_Sugar", "Blood_Pressure_Systolic", "Blood_Pressure_Diastolic", "Total_Cholesterol", "Sleep_Hours"]
-
-df[num_cols] = (df[num_cols] - df[num_cols].mean()) / df[num_cols].std()
 
 # Binary Columns
 bin_cols = ["Family_History_Diabetes", "Hypertension", "Heart_Disease", "Fatty_Liver", "PCOS"]
@@ -20,5 +20,20 @@ cat_cols = ["Physical_Activity_Level", "Diet_Quality", "Stress_Level", "Smoking_
 cols = num_cols + bin_cols + cat_cols + ["Diabetes_Risk"]
 
 df = df[cols]
-df = pd.get_dummies(df, columns=cat_cols)
+df = pd.get_dummies(df, columns=cat_cols, dtype=int)
 
+# DEFINE DATA:
+
+df_train = df.sample(frac=0.8, random_state=17)
+df_val = df.drop(index=df_train.index)
+
+y_train = df_train["Diabetes_Risk"]
+y_val = df_val["Diabetes_Risk"]
+
+X_train = df_train.drop(columns=["Diabetes_Risk"])
+X_train_mean = X_train.mean()
+X_train_std = X_train.std()
+X_train = (X_train - X_train_mean) / X_train_std
+
+X_val = df_val.drop(columns=["Diabetes_Risk"])
+X_val = (X_val - X_train_mean) / X_train_std
